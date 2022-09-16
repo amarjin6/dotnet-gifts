73a03b5bab4a082a9996a18d31ae66f9c67064db
Alexander <amarjin6@gmail.com>
2022-09-16 22:23:46 +0300

    Tracer init

---
+++ tracer/tracer/examples/Foo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example2
+{
+    public class Foo
+    {
+        private Bar _bar;
+        private ITracer _tracer;
+
+        public Foo(ITracer tracer)
+        {
+            _tracer = tracer;
+            _bar = new Bar(_tracer);
+        }
+
+        public void MyMethod()
+        {
+            _tracer.StartTrace();
+            Thread.Sleep(100);
+
+            _bar.InnerMethod();
+
+            _tracer.StopTrace();
+        }
+    }
+}

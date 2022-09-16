3a399d09a0d195425478cab8baa087e16be85bb7
Alexander <amarjin6@gmail.com>
2022-09-16 22:40:35 +0300

    Create serializers

---
+++ tracer/tracer/serializers/ITraceSerializer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tracer.Serialization
+{
+    public interface ITraceSerializer
+    {
+        string Serialize(TraceResult traceResult);
+        TraceResult Deserialize(string traceResult);
+        string GetFileExtension();
+    }
+}
---
+++ tracer/tracer/serializers/JsonSerializer.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System.Xml;
+using Tracer.Serialization;
+
+namespace Tracer.Serialization
+{
+    public class JsonTraceSerializer : ITraceSerializer
+    {
+        public string Serialize(TraceResult traceResult)
+        {
+            return JsonConvert.SerializeObject(traceResult, Formatting.Indented);
+        }
+
+        public TraceResult Deserialize(string traceResult)
+        {
+            return JsonConvert.DeserializeObject<TraceResult>(traceResult);
+        }
+
+        public string GetFileExtension()
+        {
+            return ".json";
+        }
+    }
+}
